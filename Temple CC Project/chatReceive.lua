9613dfad1b9eafa885d0599f3c1b3a7b7cf731a6
Gavin S <gavins@obsidiancorestudios.com>
2024-12-28 13:26:07 -0500

    removed junk

--- Temple CC Project/chatReceive.lua
+++
@@ -1,8 +0,0 @@
-local mod = require("http_module")
-local chatBox = peripheral.find("chatBox")
-
-while true do
-    local event, username, message, uuid, isHidden = os.pullEvent("chat")
-    local resp = mod.post("/zone/1",message)
-    chatBox.sendMessage(resp.readAll(),"Echo","<>")
-end
--- Temple CC Project/http_module.lua
+++
@@ -1,8 +0,0 @@
-local function post(path,message)
-    local headers = {["Content-Type"] = "application/json"}
-    local msg = textutils.serialiseJSON({text = message})
-    local resp = http.post("http://my-flask-app-container:5000"..path, msg, headers)
-    return resp
-end
-
-return {post = post}
