33401976f65b52e73b3c322f5e598fc6f87ae063
Gavin S <gavins@obsidiancorestudios.com>
2025-05-31 01:34:56 -0400

    feat: updated compresser to use new base64 lib

--- IrcClient/compressor.lua
+++ IrcClient/compressor.lua
@@ -1,4 +1,8 @@
+local base64 = require("base64")
+
 local function compressBlockData(blockData)
+    local block_name_lookup = {}
+
     local file = fs.open("map.json", "r")
     local map = textutils.unserialiseJSON(file.readAll())
     file.close()
@@ -22,35 +26,40 @@ local function compressBlockData(blockData)
     end
 
     local block_to_char = {
-        ["minecraft:air"] = string.char(1)
+        ["minecraft:unknown"] = "A",
+        ["minecraft:air"] = "B",
+        ["minecraft:water"] = "C"
     }
-    local next_char_code = 2
+    local next_char_code = 4
 
     for _, block in ipairs(complete_scan) do
         local name = block.name
         if not block_to_char[name] then
-            block_to_char[name] = string.char(next_char_code)
+            block_to_char[name] = base64.encode(next_char_code) or base64.encode(1)
             next_char_code = next_char_code + 1
         end
     end
 
-    local compressed = {}
-    for _, pos in ipairs(map) do
-        local key = ("%d,%d,%d"):format(pos.x, pos.y, pos.z)
-        local block = scanned_lookup[key]
-
-        if block then
-            table.insert(compressed, string.char(2)) -- not air
-        else
-            table.insert(compressed, string.char(1)) -- air
-        end
+    for name, code in pairs(block_to_char) do
+        block_name_lookup[base64.decode(code)] = name
     end
 
-    return table.concat(compressed)
+    local compressed = {}
+    for _, block in ipairs(complete_scan) do
+        table.insert(compressed, block_to_char[block.name])
+    end
+
+    return table.concat(compressed),
+    table.concat(block_name_lookup,";")
 end
 
 
-local function decompressBlockData(compressedBlocks)
+local function decompressBlockData(compressedBlocks, compressedBlockLookup)
+    local block_lookup = {}
+    for name in string.gmatch(compressedBlockLookup, "([^;]+)") do
+        table.insert(block_lookup,name)
+    end
+
     local file = fs.open("map.json","r")
     local json_map = file.readAll()
     file.close()
@@ -58,13 +67,13 @@ local function decompressBlockData(compressedBlocks)
 
     local decompressedBlocksRaw = {}
     for i = 1, #compressedBlocks do
-        decompressedBlocksRaw[i] = string.byte(string.sub(compressedBlocks,i,i))
+        decompressedBlocksRaw[i] = base64.decode(string.sub(compressedBlocks,i,i))
     end
 
     local decompressedBlocks = {}
     for i = 1, #decompressedBlocksRaw do
         decompressedBlocks[i] = {}
-        decompressedBlocks[i].name = decompressedBlocksRaw[i]
+        decompressedBlocks[i].name = block_lookup[decompressedBlocksRaw[i]]
         decompressedBlocks[i].x = map[i].x
         decompressedBlocks[i].y = map[i].y
         decompressedBlocks[i].z = map[i].z
