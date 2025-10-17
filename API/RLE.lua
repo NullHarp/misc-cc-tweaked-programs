64438d98c1d1f85f73e9f391b1972ed6c08b9a86
Gavin S <gavins@obsidiancorestudios.com>
2025-10-17 13:05:59 -0400

    feat: updated turtleIRC

--- API/RLE.lua
+++ API/RLE.lua
@@ -8,10 +8,19 @@ local function encode(data)
     local last_char = " "
     for i = 1, #data+1 do
         local data_char = string.sub(data,i,i)
-        if data_char == last_char and running_total < 64 then
+        if data_char == last_char and running_total < 4095 then
             running_total = running_total + 1
         elseif last_char ~= " " then
-            local encoded_str = base64.encode(running_total)
+            local encoded_str = ""
+            if running_total == 63 then
+                encoded_str = "A/"
+            elseif running_total > 63 then
+                local len = math.floor(running_total/64)
+                local len_2 = running_total%64
+                encoded_str = base64.encode(len+1)..base64.encode(len_2+1)
+            else
+                encoded_str = "A"..base64.encode(running_total)
+            end
             encoded_str = encoded_str .. last_char
             running_total = 1
             result = result .. encoded_str
@@ -23,11 +32,13 @@ end
 
 local function decode(data)
     local result = ""
-    for i = 1, #data, 2 do
+    for i = 1, #data, 3 do
         local len_char = string.sub(data,i,i)
-        local data_char = string.sub(data,i+1,i+1)
+        local len_char_2 = string.sub(data,i+1,i+1)
+        local data_char = string.sub(data,i+2,i+2)
         local len = base64.decode(len_char)
-        local decomp = string.rep(data_char,len)
+        local len_2 = base64.decode(len_char_2)
+        local decomp = string.rep(data_char,((len-1)*64)+len_2-1)
         result = result .. decomp
     end
     return result
